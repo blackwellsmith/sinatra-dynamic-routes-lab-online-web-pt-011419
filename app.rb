0453cbaa6c8cdbd6d60ccfe51cb2cf4c9b18eede
blackwell <blackwellsmithiv@gmail.com>
2019-04-16 20:41:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,12 +24,12 @@ class App < Sinatra::Base
     operator = params[:operation]
     num1 = params[:number1].to_i
     num2 = params[:number2].to_i
-    case operator
     
+    
+    case operator
     when "add"
       (num1 + num2).to_s
     when "subtract"
-    
       (num1 - num2).to_s
     when "multiply"
       (num1 * num2).to_s
